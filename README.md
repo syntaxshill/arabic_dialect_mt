df916dd987bd3442369dd34c7e08c1842fa1e227
Ibrahim Sharaf <ibrahim.sharaf@cision.com>
2024-06-03 14:38:17 +0200

    Update AraT5 evals

--- README.md
+++ README.md
@@ -18,8 +18,19 @@ $ python -m src.benchmark_llms data_file [--gpt] [--aya] [--gemini]
 ```
 
 
-## AraT5 finetuning and evaluation
+## AraT5 translation and evaluation
+The following script performs translation using fine-tuned AraT5 model, then sentence-level evaluations and dialect-level evaluations.
 
+We uploaded each of our AraT5 trained checkpoints to huggingface hub, we employed the three stage fine-tuning approach.
 
-## LLM backtranslation, AraT5 finetuning, and evaluation
+To reproduce AraT5 results you can use the eval script for the three stages
 
+```shell
+$ python -m src.inference_AraT5 --model_name "ibrahimsharaf/AraT5_stage1" --input_data "data/osact6_task2_test.csv" --batch_size 32
+$ python -m src.inference_AraT5 --model_name "ibrahimsharaf/AraT5_stage2" --input_data "data/osact6_task2_test.csv" --batch_size 32
+$ python -m src.inference_AraT5 --model_name "ibrahimsharaf/AraT5_stage3" --input_data "data/osact6_task2_test.csv" --batch_size 32
+```
+
+AraT5 translations and sentence-level evaluations are stored at `out/{model_name}_{data_file}.csv`.
+
+Dialect-level evaluation is saved at `metrics/{model_name}_{data_file}.csv`.
--- src/inference_AraT5.py
+++ src/inference_AraT5.py
@@ -1,51 +1,99 @@
-import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
+import argparse
 import pandas as pd
+import numpy as np
 from tqdm import tqdm
+import random
+import warnings
+
+warnings.filterwarnings('ignore')
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
+
+from src.evaluation import do_sentence_evals, do_dialect_eval
 
 
-tokenizer = AutoTokenizer.from_pretrained("checkpoints/madar")
-model = AutoModelForSeq2SeqLM.from_pretrained("checkpoints/madar").to("cuda")
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
 
 
-def get_prefix(lang):
-    if lang == "Egyptian":
+def add_prefix(dialect):
+    if dialect == "Egyptian":
         return "ترجم من اللهجة المصرية إلى اللغة العربية الفصحى: "
-    elif lang == "Gulf":
+    elif dialect == "Gulf":
         return "ترجم من اللهجة الخليجية إلى اللغة العربية الفصحى: "
-    elif lang == "Levantine":
+    elif dialect == "Levantine":
         return "ترجم من اللهجة الشامية إلى اللغة العربية الفصحى: "
-    elif lang == "Iraqi":
+    elif dialect == "Iraqi":
         return "ترجم من اللهجة العراقية إلى اللغة العربية الفصحى: "
-    elif lang == "Magharebi":
+    elif dialect == "Magharebi":
         return "ترجم من اللهجة المغاربية إلى اللغة العربية الفصحى: "
     else:
         return ""
 
 
-def predict(batch):
+def predict(batch, model, tokenizer, device):
     sources = batch["source"]
-    langs = batch["dialect"]
-    prefixes = [get_prefix(lang) for lang in langs]
+    dialects = batch["dialect"]
+    prefixes = [add_prefix(dialect) for dialect in dialects]
     inputs = [prefix + source for prefix, source in zip(prefixes, sources)]
 
     tokenized_inputs = tokenizer(inputs, return_tensors="pt", padding=True, truncation=True)
-    input_ids = tokenized_inputs.input_ids.to("cuda")
-    attention_mask = tokenized_inputs.attention_mask.to("cuda")    
+    input_ids = tokenized_inputs.input_ids.to(device)
+    attention_mask = tokenized_inputs.attention_mask.to(device)
     with torch.no_grad():
-        outputs = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=40, do_sample=True, top_k=30, top_p=0.95)
+        outputs = model.generate(input_ids, attention_mask=attention_mask,
+                                 max_new_tokens=40, do_sample=True, top_k=30,
+                                 top_p=0.95)
     decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
     return decoded_outputs
 
 
-osact = pd.read_csv("data/osact6_task2_test.csv")
-batch_size = 32
-osact["translation"] = ""
+def main(args):
+    set_seed(42)  # to reproduce results
 
-for i in tqdm(range(0, len(osact), batch_size)):
-    batch = osact[i:i+batch_size]
-    translations = predict(batch)
-    osact.loc[i:i+batch_size-1, "translation"] = translations
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-osact.to_csv("out/madar_osact_with_translations.csv", index=False)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name).to(device)
+
+    dataset_name = args.input_data.split("/")[-1].split(".")[0]
+    model_name = args.model_name.split("/")[-1]
+
+    osact = pd.read_csv(args.input_data)
+    tqdm.pandas()
+
+    osact[model_name] = ""
+
+    for i in tqdm(range(0, len(osact), args.batch_size)):
+        batch = osact[i:i + args.batch_size]
+        translations = predict(batch, model, tokenizer, device)
+        osact.loc[i:i + args.batch_size - 1, model_name] = translations
+
+    print("Evaluating...")
+    df = do_sentence_evals(osact, mt_col=model_name)
+
+    output_file = f"out/{model_name}_{dataset_name}.csv"
+    df.to_csv(output_file, index=False)
+    print(f"Saved at {output_file}")
+
+    print("Performing dialect-level evaluation...")
+
+    metrics_df = do_dialect_eval(df, mt_col=model_name, all_dialects=True)
+    metrics_file = f"metrics/{model_name}_{dataset_name}.csv"
+    metrics_df.to_csv(metrics_file, index=False)
+    print(f"Saved at {metrics_file}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Translate Arabic dialects to MSA using a pretrained model.")
+    parser.add_argument("--model_name", type=str, help="Model checkpoint name on huggingface hub.")
+    parser.add_argument("--input_data", type=str, help="Path to input dataset.")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for translation.")
+
+    args = parser.parse_args()
+    main(args)
